a376d7514bdefba8ce44decaa63697475aa61352
longino22 <nlongn22@icloud.com>
2021-11-22 22:17:48 +0100

    Add screenshot functionality & type into fields

--- main.py
+++ main.py
@@ -1,18 +1,13 @@
 import pyautogui
-from pynput import keyboard
+from pynput import keyboard, mouse
 from PIL import Image
 import pytesseract
+import time
 
-
-# pytesseract.pytesseract.tesseract_cmd = "C:\\Program Files\\Tesseract-OCR\\tesseract"
-# screenshot = pyautogui.screenshot()
-# screenshot.save(r"./screenshot.png")
-
-# image = Image.open("screenshot.png")
-# text = pytesseract.image_to_string(image)
-# print(text)
+pytesseract.pytesseract.tesseract_cmd = "C:\\Program Files\\Tesseract-OCR\\tesseract"
 
 x1, y1, x2, y2 = 0, 0, 0, 0
+second_click = False
 
 
 def set_coordinates(has_run):
@@ -25,28 +20,40 @@ def set_coordinates(has_run):
         print(f"Second coordinates: {x2}, {y2}")
 
 
-def get_delta():
-    global x1, y1, x2, y2
-    deltaX = x2 - x1
-    deltaY = y2 - y1
-    print(f"Delta of coordinates: {deltaX}, {deltaY}")
+# def get_delta():
+#     global x1, y1, x2, y2
+#     deltaX = x2 - x1
+#     deltaY = y2 - y1
+#     print(f"Delta of coordinates: {deltaX}, {deltaY}")
 
 
-def on_press(key):
+def on_click(x, y, button, pressed):
+    global second_click
     try:
-        if key.char == "1":
+        if pressed:
             set_coordinates(True)
-
-        if key.char == "2":
+        if not pressed:
             set_coordinates(False)
-            get_delta()
-
-        if key.char == "q" or key.char == "s" or key.char == "e":
-            print("Program stopped")
             listener.stop()
     except AttributeError:
         pass
 
 
-with keyboard.Listener(on_press=on_press) as listener:
+with mouse.Listener(on_click=on_click) as listener:
     listener.join()
+
+screenshot = pyautogui.screenshot(region=(x1, y1, x2 - x1, y2 - y1))
+screenshot.save(r"./test.png")
+
+image = Image.open("test.png")
+text = pytesseract.image_to_string(image)
+text = text.replace("\n", " ")
+text = text.replace("|", "")
+text = " ".join(text.split())
+print(text)
+
+list_of_chars = list(text)
+time.sleep(3)
+for char in list_of_chars:
+    keyboard.Controller().press(char)
+    time.sleep(0.1)
