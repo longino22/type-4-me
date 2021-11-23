47dc2f06df331b76d84db36925ed6a20982bad4e
longino22 <nlongn22@icloud.com>
2021-11-23 22:21:00 +0100

    Add GUI window

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import pyautogui
 from pynput import keyboard, mouse
-from PIL import Image
+import PIL.Image
 import pytesseract
 import time
+import tkinter
+from tkinter import *
 
 pytesseract.pytesseract.tesseract_cmd = "C:\\Program Files\\Tesseract-OCR\\tesseract"
 
 x1, y1, x2, y2 = 0, 0, 0, 0
-second_click = False
 
 
 def set_coordinates(has_run):
@@ -20,27 +21,21 @@ def set_coordinates(has_run):
         print(f"Second coordinates: {x2}, {y2}")
 
 
-# def get_delta():
-#     global x1, y1, x2, y2
-#     deltaX = x2 - x1
-#     deltaY = y2 - y1
-#     print(f"Delta of coordinates: {deltaX}, {deltaY}")
+def select_area():
+    def on_click(x, y, button, pressed):
+        global second_click
+        try:
+            if pressed:
+                set_coordinates(True)
+            if not pressed:
+                set_coordinates(False)
+                listener.stop()
+                take_screenshot()
+        except AttributeError:
+            pass
 
-
-def on_click(x, y, button, pressed):
-    global second_click
-    try:
-        if pressed:
-            set_coordinates(True)
-        if not pressed:
-            set_coordinates(False)
-            listener.stop()
-    except AttributeError:
-        pass
-
-
-with mouse.Listener(on_click=on_click) as listener:
-    listener.join()
+    with mouse.Listener(on_click=on_click) as listener:
+        listener.join()
 
 
 def take_screenshot():
@@ -48,13 +43,10 @@ def take_screenshot():
     screenshot.save(r"./test.png")
 
 
-take_screenshot()
-
-
 def convert_to_text():
-    init_image = Image.open("test.png").convert("L")
+    init_image = PIL.Image.open("test.png").convert("L")
     init_image.save("grey.png")
-    grey_image = Image.open("grey.png")
+    grey_image = PIL.Image.open("grey.png")
     text = pytesseract.image_to_string(grey_image)
     text = text.replace("\n", " ")
     text = text.replace("|", "")
@@ -62,8 +54,8 @@ def convert_to_text():
     print(text)
     return text
 
-
 def type():
+    time.sleep(3)
     list_of_chars = list(convert_to_text())
     for char in list_of_chars:
         if char == "?":
@@ -76,5 +68,12 @@ def type():
         type()
 
 
-time.sleep(3)
-type()
+root = tkinter.Tk()
+root.geometry("150x150")
+
+select_button = Button(root, text="Select area", command=select_area)
+select_button.place(x=75, y=50, anchor=CENTER)
+
+start_button = Button(root, text="Start typing", command=type)
+start_button.place(x=75, y=100, anchor=CENTER)
+root.mainloop()
